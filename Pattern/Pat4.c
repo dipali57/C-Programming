1d84a35a69f00775a655a535bb6ef19e823f6dc7
Dipali Gunjal <60472255+dipali57@users.noreply.github.com>
2021-09-07 11:59:41 +0530

    Printing Pyramid Patterns

---
+++ Pattern/Pat4.c
@@ -0,0 +1,50 @@
+/*
+input row=4 col=4
+output	
+* * * *
+  * * *
+    * *
+      *
+
+*/
+#include<stdio.h>
+void Pattern(int iRow,int iCol)
+{
+
+	int i=0,j=0;
+	if(iRow!=iCol)	
+	{
+		printf("Row and column value must be same");
+		return;
+	}
+	else
+	{
+		for(i=0;i<iRow;i++)
+		{
+			for(j=0;j<iCol;j++)
+			{
+				if(j>=i)
+					printf("*");
+				else
+					printf(" ");
+			
+			}
+		printf("\n");
+		}
+	}
+	
+}
+int main()
+{
+	int iR=0,iC=0;
+
+	printf("Enter Row:");
+	scanf("%d",&iR);
+
+	printf("Enter Col:");
+	scanf("%d",&iC);
+
+	Pattern(iR,iC);
+
+	return 0;
+}
